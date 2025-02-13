b8fba50471e9d18dc85c74af66c61fbfdc55ed4b
pchchv <ipchchv@gmail.com>
2025-02-13 10:18:17 +0300

    Add command struct to encapsulate server command-line options

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -2,6 +2,8 @@ package server
 
 import (
 	"context"
+	"flag"
+	"os"
 
 	"github.com/gorilla/websocket"
 )
@@ -10,4 +12,25 @@ type Fileserver interface {
 	Setup(pathToMaster string) (string, error)
 	Start(ctx context.Context) error
 	WsHandler(ws *websocket.Conn)
+}
+
+type command struct {
+	port         *int
+	wsPath       *string
+	binPath      string
+	tlsKeyPath   *string
+	masterPath   string
+	mirrorPath   string
+	tlsCertPath  *string
+	cacheControl *string
+	forceReload  *bool
+	fileserver   Fileserver
+	flagset      *flag.FlagSet
+}
+
+func Command() *command {
+	r, _ := os.Executable()
+	return &command{
+		binPath: r,
+	}
 }
