797fac27c6911fee242814dafdc1944bb4b93be4
pchchv <ipchchv@gmail.com>
2025-02-13 09:38:19 +0300

    Implement printHelp function for enhanced error handling and usage display

--- main.go
+++ main.go
@@ -1,6 +1,33 @@
 package main
 
-import "github.com/pchchv/sws/helpers"
+import (
+	"errors"
+	"fmt"
+
+	"github.com/pchchv/sws/cmd"
+	"github.com/pchchv/sws/helpers"
+)
+
+func printHelp(command cmd.Command, err error, printUsage cmd.UsagePrinter) int {
+	var notValidArg cmd.ArgNotFoundError
+	if errors.As(err, &notValidArg) {
+		helpers.PrintErr(err.Error())
+		printUsage()
+	} else if errors.Is(err, cmd.ErrNoArgs) {
+		printUsage()
+	} else if errors.Is(err, cmd.ErrHelpful) {
+		if command != nil {
+			fmt.Println(command.Help())
+		} else {
+			printUsage()
+		}
+		return 0
+	} else {
+		helpers.PrintfErr("unknown error: %v", err.Error())
+	}
+
+	return 1
+}
 
 func main() {
 	helpers.Newline = true
