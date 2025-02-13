2650d646d2406c694f60274b8dfdc5dfa29f4662
pchchv <ipchchv@gmail.com>
2025-02-13 09:18:20 +0300

    Implement logging functionality in ansi_print helper

--- helpers/ansi_print.go
+++ helpers/ansi_print.go
@@ -1,12 +1,32 @@
 package helpers
 
 import (
+	"bytes"
 	"context"
+	"fmt"
 	"log/slog"
+	"os"
+	"time"
 )
 
 type ansiprint struct{}
 
 func (a *ansiprint) Enabled(context.Context, slog.Level) bool {
 	return true
+}
+
+func (a *ansiprint) Handle(ctx context.Context, r slog.Record) error {
+	var bf bytes.Buffer
+	if !r.Time.IsZero() {
+		fmt.Fprintf(&bf, "%v %v", r.Time.Format(time.RFC3339), r.Message)
+	}
+
+	switch r.Level {
+	case slog.LevelDebug, slog.LevelWarn, slog.LevelInfo:
+		fmt.Fprint(os.Stdout, bf.String())
+	case slog.LevelError:
+		fmt.Fprint(os.Stderr, bf.String())
+	}
+
+	return nil
 }
