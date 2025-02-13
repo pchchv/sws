d722cce79688cc1b4f715c52b41772ce11b4c15d
pchchv <ipchchv@gmail.com>
2025-02-13 09:58:43 +0300

    Add unit test for Run method in Command struct to verify version output

--- cmd/version/version_test.go
+++ cmd/version/version_test.go
@@ -1,6 +1,13 @@
 package version
 
-import "testing"
+import (
+	"bytes"
+	"context"
+	"debug/buildinfo"
+	"os"
+	"runtime/debug"
+	"testing"
+)
 
 func TestCommand(t *testing.T) {
 	cmd := Command()
@@ -19,4 +26,42 @@ func TestCommand(t *testing.T) {
 	if help := cmd.Help(); help != "Print the version of sws" {
 		t.Fatalf("Unexpected help output: %v", help)
 	}
+}
+
+func TestRun(t *testing.T) {
+	cmd := Command()
+	ctx := context.Background()
+	t.Run("it should print version info correctly", func(t *testing.T) {
+		cmd.getVersionCmd = func() (*buildinfo.BuildInfo, bool) {
+			return &buildinfo.BuildInfo{
+				Main: debug.Module{
+					Version: "v1.2.3",
+					Sum:     "h1:checksum",
+				},
+				GoVersion: "go1.23.5",
+			}, true
+		}
+
+		oldStdout := os.Stdout
+		r, w, _ := os.Pipe()
+		os.Stdout = w
+
+		err := cmd.Run(ctx)
+		w.Close()
+		os.Stdout = oldStdout
+
+		if err != nil {
+			t.Fatalf("Run failed: %v", err)
+		}
+
+		// Читаем вывод
+		var buf bytes.Buffer
+		_, _ = buf.ReadFrom(r)
+		got := buf.String()
+
+		expected := "version: v1.2.3, go version: go1.23.5, checksum: h1:checksum\n"
+		if got != expected {
+			t.Fatalf("Expected output %q, got %q", expected, got)
+		}
+	})
 }
