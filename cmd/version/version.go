03e167eab55f32fb45c8c839f99610d4fadefddb
pchchv <ipchchv@gmail.com>
2025-02-13 09:45:05 +0300

    Add Setup and Help methods to command struct for version handling

--- cmd/version/version.go
+++ cmd/version/version.go
@@ -5,3 +5,14 @@ import "runtime/debug"
 type command struct {
 	getVersionCmd func() (*debug.BuildInfo, bool)
 }
+
+// Setup sets up the *command.
+func (c *command) Setup() error {
+	c.getVersionCmd = debug.ReadBuildInfo
+	return nil
+}
+
+// Help helps by printing out the help.
+func (c *command) Help() string {
+	return "Print the version of wd-41"
+}
