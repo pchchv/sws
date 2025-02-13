1e05915b596d683e86c7d9bf15432d1f97abba55
pchchv <ipchchv@gmail.com>
2025-02-13 09:48:33 +0300

    Add Run method to command struct for version printing and error handling

--- cmd/version/version.go
+++ cmd/version/version.go
@@ -1,6 +1,17 @@
 package version
 
-import "runtime/debug"
+import (
+	"context"
+	"errors"
+	"fmt"
+	"runtime/debug"
+)
+
+// Set with buildflag if the build is done in the pipeline, not with go install.
+var (
+	BUILD_VERSION  = ""
+	BUILD_CHECKSUM = ""
+)
 
 type command struct {
 	getVersionCmd func() (*debug.BuildInfo, bool)
@@ -15,4 +26,26 @@ func (c *command) Setup() error {
 // Help helps by printing out the help.
 func (c *command) Help() string {
 	return "Print the version of wd-41"
+}
+
+// Run runs the *command, printing the version using either debugbuild or tagged version.
+func (c *command) Run(context.Context) error {
+	bi, ok := c.getVersionCmd()
+	if !ok {
+		return errors.New("failed to read build info")
+	}
+
+	version := bi.Main.Version
+	if version == "" || version == "(devel)" {
+		version = BUILD_VERSION
+	}
+
+	checksum := bi.Main.Sum
+	if checksum == "" {
+		checksum = BUILD_CHECKSUM
+	}
+
+	fmt.Printf("version: %v, go version: %v, checksum: %v\n", version, bi.GoVersion, checksum)
+
+	return nil
 }
