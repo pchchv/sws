f80404d4e83a16a2be147e518482efc75b0f5ee3
pchchv <ipchchv@gmail.com>
2025-02-13 09:26:19 +0300

    Add colorCode type and constants for ANSI color support

--- helpers/ancli.go
+++ helpers/ancli.go
@@ -12,6 +12,17 @@ var (
 	Newline = false || strings.ToLower(os.Getenv("ANCLI_NEWLINE")) == "true"
 )
 
+type colorCode int
+
+const (
+	RED colorCode = iota + 31
+	GREEN
+	YELLOW
+	BLUE
+	MAGENTA
+	CYAN
+)
+
 func SetupSlog() {
 	slogger = slog.New(&ansiprint{})
 	SlogIt = true
