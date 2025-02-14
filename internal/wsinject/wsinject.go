6d9da843d11d897eb707796282e69b8fd589cd08
pchchv <ipchchv@gmail.com>
2025-02-14 10:11:42 +0300

    Add error handling and HTML injection functionality in wsinject package

--- internal/wsinject/wsinject.go
+++ internal/wsinject/wsinject.go
@@ -1,11 +1,20 @@
 package wsinject
 
 import (
+	"bytes"
+	"errors"
+	"fmt"
+	"io/fs"
+	"log"
 	"os"
+	"path/filepath"
+	"strings"
 	"sync"
 
 	"github.com/fsnotify/fsnotify"
 )
+
+var ErrNoHeaderTagFound = errors.New("no header tag found")
 
 type Fileserver struct {
 	masterPath            string
@@ -39,4 +48,36 @@ func NewFileServer(wsPort int, wsPath string, forceReload, expectTLS bool) *File
 		wsDispatcherStarted:   &started,
 		wsDispatcherStartedMu: &sync.Mutex{},
 	}
+}
+
+func wsInjectMaster(root string, do func(path string, d fs.DirEntry, err error) error) error {
+	if err := filepath.WalkDir(root, do); err != nil {
+		log.Fatalf("Error walking the path %q: %v\n", root, err)
+	}
+	return nil
+}
+
+func injectScript(html []byte, scriptTag string) ([]byte, error) {
+	htmlStr := string(html)
+	// find the location of the closing `</header>` tag
+	idx := strings.Index(htmlStr, "</head>")
+	if idx == -1 {
+		return html, ErrNoHeaderTagFound
+	}
+
+	var buf bytes.Buffer
+	// write the HTML up to the closing `</head>` tag
+	if _, err := buf.WriteString(htmlStr[:idx]); err != nil {
+		return nil, fmt.Errorf("failed to write pre: %w", err)
+	}
+
+	if _, err := buf.WriteString(scriptTag); err != nil {
+		return nil, fmt.Errorf("failed to write script tag: %w", err)
+	}
+
+	if _, err := buf.WriteString(htmlStr[idx:]); err != nil {
+		return nil, fmt.Errorf("failed to write post: %w", err)
+	}
+
+	return buf.Bytes(), nil
 }
