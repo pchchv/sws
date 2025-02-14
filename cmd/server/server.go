827dd290dc3a3e2f310343c13626852331b389ac
pchchv <ipchchv@gmail.com>
2025-02-14 12:13:47 +0300

    Implement Run method in command to set up and manage the HTTP server with WebSocket support

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -2,12 +2,15 @@ package server
 
 import (
 	"context"
+	"errors"
 	"flag"
 	"fmt"
+	"net/http"
 	"os"
 	"path"
 
 	"github.com/gorilla/websocket"
+	"github.com/pchchv/sws/helpers/ancli"
 	"github.com/pchchv/sws/internal/wsinject"
 )
 
@@ -82,4 +85,82 @@ func (c *command) Flagset() *flag.FlagSet {
 	c.tlsKeyPath = fs.String("tlsKeyPath", "", "set to a path to a key, requires tlsCertPath to be set")
 	c.flagset = fs
 	return fs
+}
+
+func (c *command) Run(ctx context.Context) (err error) {
+	mux := http.NewServeMux()
+	fsh := http.FileServer(http.Dir(c.mirrorPath))
+	fsh = slogHandler(fsh)
+	fsh = cacheHandler(fsh, *c.cacheControl)
+	fsh = crossOriginIsolationHandler(fsh)
+	mux.Handle("/", fsh)
+
+	ancli.PrintfOK("setting up websocket host on path: '%v'", *c.wsPath)
+	mux.HandleFunc(*c.wsPath, func(w http.ResponseWriter, r *http.Request) {
+		upgrader := websocket.Upgrader{}
+		conn, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			http.Error(w, "Failed to upgrade to websocket", http.StatusInternalServerError)
+			return
+		}
+		c.fileserver.WsHandler(conn)
+	})
+
+	s := http.Server{
+		Addr:        fmt.Sprintf(":%v", *c.port),
+		Handler:     mux,
+		ReadTimeout: 0,
+	}
+	serverErrChan := make(chan error, 1)
+	fsErrChan := make(chan error, 1)
+	go func() {
+		serveTLS := *c.tlsCertPath != "" && *c.tlsKeyPath != ""
+		hostname := "localhost"
+		protocol := "http"
+		if serveTLS {
+			protocol = "https"
+		}
+		baseURL := fmt.Sprintf("%s://%s:%d", protocol, hostname, *c.port)
+
+		ancli.PrintfOK("Server started successfully:")
+		ancli.PrintfOK("- URL: %s", baseURL)
+		ancli.PrintfOK("- Serving directory: '%v'", c.masterPath)
+		ancli.PrintfOK("- Mirror directory: '%v'", c.mirrorPath)
+		if serveTLS {
+			ancli.PrintfOK("- TLS enabled (cert: '%v', key: '%v')", *c.tlsCertPath, *c.tlsKeyPath)
+		} else {
+			ancli.PrintfOK("- TLS disabled")
+		}
+
+		if serveTLS {
+			err = s.ListenAndServeTLS(*c.tlsCertPath, *c.tlsKeyPath)
+		} else {
+			err = s.ListenAndServe()
+		}
+
+		if !errors.Is(err, http.ErrServerClosed) {
+			serverErrChan <- err
+		}
+	}()
+	go func() {
+		ancli.PrintOK("starting fsnotify file detector")
+		if err := c.fileserver.Start(ctx); err != nil {
+			fsErrChan <- err
+		}
+	}()
+
+	select {
+	case <-ctx.Done():
+	case serveErr := <-serverErrChan:
+		err = serveErr
+		break
+	case fsErr := <-fsErrChan:
+		err = fsErr
+		break
+	}
+
+	ancli.PrintNotice("initiating webserver graceful shutdown")
+	s.Shutdown(ctx)
+	ancli.PrintOK("shutdown complete")
+	return err
 }
