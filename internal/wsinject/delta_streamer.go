9edb06c1a8c0754d39b7e72e52f2883558f1e22d
pchchv <ipchchv@gmail.com>
2025-02-14 11:03:40 +0300

    Add mutex-based read and write functions for thread-safe operations

--- internal/wsinject/delta_streamer.go
+++ internal/wsinject/delta_streamer.go
@@ -1,6 +1,10 @@
 package wsinject
 
-import "github.com/pchchv/sws/helpers/ancli"
+import (
+	"sync"
+
+	"github.com/pchchv/sws/helpers/ancli"
+)
 
 func (fs *Fileserver) wsDispatcherStart() {
 	for {
@@ -24,4 +28,18 @@ func (fs *Fileserver) wsDispatcherStart() {
 			return true
 		})
 	}
+}
+
+// Reads by locking the mutex before taking a copy, will then return the copy.
+func read[T any](m *sync.Mutex, src *T) T {
+	m.Lock()
+	defer m.Unlock()
+	return *src
+}
+
+// Writes by locking the mutex before writing.
+func write[T any](m *sync.Mutex, value T, dest *T) {
+	m.Lock()
+	defer m.Unlock()
+	*dest = value
 }
