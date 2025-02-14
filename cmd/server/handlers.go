d994bacb60c89f2179bf9ad1862e2ec4ef9f57f1
pchchv <ipchchv@gmail.com>
2025-02-14 12:09:11 +0300

    Add middleware handlers for logging, caching, and cross-origin isolation

---
+++ cmd/server/handlers.go
@@ -0,0 +1,29 @@
+package server
+
+import (
+	"net/http"
+
+	"github.com/pchchv/sws/helpers/ancli"
+)
+
+func slogHandler(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ancli.PrintfOK("%s - %s", r.Method, r.URL.Path)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func cacheHandler(next http.Handler, cacheControl string) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Cache-Control", cacheControl)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func crossOriginIsolationHandler(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Cross-Origin-Opener-Policy", "same-origin")
+		w.Header().Add("Cross-Origin-Embedder-Policy", "require-corp")
+		next.ServeHTTP(w, r)
+	})
+}
