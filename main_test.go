3898b3212b31a473468684d37670b87679f0078a
pchchv <ipchchv@gmail.com>
2025-02-15 14:44:26 +0300

    Add unit tests for Run function

--- main_test.go
+++ main_test.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"context"
+	"errors"
 	"flag"
+	"testing"
+
+	"github.com/pchchv/sws/cmd"
 )
 
 type MockCommand struct {
@@ -29,4 +33,54 @@ func (m MockCommand) Setup() error {
 
 func (m MockCommand) Flagset() *flag.FlagSet {
 	return flag.NewFlagSet("test", flag.ContinueOnError)
+}
+
+func Test_Run_ExitCodes(t *testing.T) {
+	tests := []struct {
+		name      string
+		args      []string
+		argParser cmd.ArgParser
+		expected  int
+	}{
+		{
+			name: "on invalid arg, it should return exit code 1",
+			args: []string{"invalid"},
+			argParser: func(s []string) (cmd.Command, error) {
+				return nil, errors.New("some error")
+			},
+			expected: 1,
+		},
+		{
+			name: "on run error, it should return exit code 1",
+			args: []string{"valid"},
+			argParser: func(s []string) (cmd.Command, error) {
+				return MockCommand{
+					runFunc: func(ctx context.Context) error {
+						return errors.New("whopsidops, error ojoj..!")
+					},
+				}, nil
+			},
+			expected: 1,
+		},
+		{
+			name: "on success, error code 0",
+			args: []string{"valid"},
+			argParser: func(s []string) (cmd.Command, error) {
+				return MockCommand{
+					runFunc: func(ctx context.Context) error {
+						return nil
+					},
+				}, nil
+			},
+			expected: 0,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if result := run(context.Background(), tt.args, tt.argParser); result != tt.expected {
+				t.Errorf("run() = %v, want %v", result, tt.expected)
+			}
+		})
+	}
 }
