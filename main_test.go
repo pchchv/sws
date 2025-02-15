9392c868a73aee9bf5b874aa3183f8557357605f
pchchv <ipchchv@gmail.com>
2025-02-15 14:43:24 +0300

    Implement methods Run, Help, Describe, Setup and Flagset

--- main_test.go
+++ main_test.go
@@ -1,9 +1,32 @@
 package main
 
-import "context"
+import (
+	"context"
+	"flag"
+)
 
 type MockCommand struct {
 	runFunc      func(context.Context) error
 	helpFunc     func() string
 	describeFunc func() string
+}
+
+func (m MockCommand) Run(ctx context.Context) error {
+	return m.runFunc(ctx)
+}
+
+func (m MockCommand) Help() string {
+	return m.helpFunc()
+}
+
+func (m MockCommand) Describe() string {
+	return m.describeFunc()
+}
+
+func (m MockCommand) Setup() error {
+	return nil
+}
+
+func (m MockCommand) Flagset() *flag.FlagSet {
+	return flag.NewFlagSet("test", flag.ContinueOnError)
 }
