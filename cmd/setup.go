4f4ac5df2f1e13ce874d6ea8733280fbc41a1f52
pchchv <ipchchv@gmail.com>
2025-02-15 14:08:16 +0300

    Implement functions isHelp and PrintUsage

--- cmd/setup.go
+++ cmd/setup.go
@@ -9,9 +9,26 @@ import (
 	"github.com/pchchv/sws/cmd/version"
 )
 
+const usage = `== Web Development 41 ==
+
+This tool is designed to enable live reload for statically hosted web development.
+It injects a websocket script in a mirrored version of html pages
+and uses the fsnotify (cross-platform 'inotify' wrapper) package to detect filechanges.
+On filechanges, the websocket will trigger a reload of the page.
+
+The 41 (formerly "40", before I got spooked by potential lawyers) is only
+to enable rust-repellant properties.
+
+Commands:
+%v`
+
 var commands = map[string]Command{
 	"s|serve":   server.Command(),
 	"v|version": version.Command(),
+}
+
+func PrintUsage() {
+	fmt.Printf(usage, formatCommandDescriptions())
 }
 
 func formatCommandDescriptions() string {
@@ -22,4 +39,8 @@ func formatCommandDescriptions() string {
 	}
 	w.Flush()
 	return buf.String()
+}
+
+func isHelp(s string) bool {
+	return s == "-h" || s == "-help" || s == "h" || s == "help"
 }
